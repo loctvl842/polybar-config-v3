caeaef6c94215043591ee32398b157a959e3c4f0
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-06-01 10:09:59 +0700

    add colorful single bar

--- Multipart/launch.sh
+++ Multipart/launch.sh
@@ -5,11 +5,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-export BAR_LABEL_SEPARATOR="%{T5}%{F$(xrdb -query | grep "*.color14" | cut -f 2)}%{F-}%{T-}"
-
 polybar topbar-left -r -c ~/.config/polybar/Multipart/config.ini &
 polybar topbar-center -r -c ~/.config/polybar/Multipart/config.ini &
 polybar topbar-right -r -c ~/.config/polybar/Multipart/config.ini &
 polybar topbar-beast -r -c ~/.config/polybar/Multipart/config.ini &
 polybar topbar-tray -r -c ~/.config/polybar/Multipart/config.ini &
-polybar container -r -c ~/.config/polybar/Multipart/container/container.ini &
--- Single-white/launch.sh
+++ Single-white/launch.sh
@@ -3,4 +3,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar single -r -c ~/.config/polybar/Single/config.ini &
+polybar single -r -c ~/.config/polybar/Single-white/config.ini &
