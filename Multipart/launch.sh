04d5c96b28bd71372a1898069390df5c754dfe35
locletvl842002 <loc.letvl@hcmut.edu.vn>
2022-07-27 09:35:54 +0700

    add configuration for external monitor

--- Multipart/launch.sh
+++ Multipart/launch.sh
@@ -1,10 +1,23 @@
 sleep 0.1
-polybar topbar-left -r -c ~/.config/polybar/Multipart/config.ini &
+polybar topbar-left -r -c ~/.config/polybar/Multipart/main/config.ini &
 sleep 0.1
-polybar topbar-tray -r -c ~/.config/polybar/Multipart/config.ini &
+polybar topbar-tray -r -c ~/.config/polybar/Multipart/main/config.ini &
 sleep 0.1
-polybar topbar-center -r -c ~/.config/polybar/Multipart/config.ini &
+polybar topbar-center -r -c ~/.config/polybar/Multipart/main/config.ini &
 sleep 0.1
-polybar topbar-beast -r -c ~/.config/polybar/Multipart/config.ini &
+polybar topbar-beast -r -c ~/.config/polybar/Multipart/main/config.ini &
 sleep 0.1
-polybar topbar-right -r -c ~/.config/polybar/Multipart/config.ini &
+polybar topbar-right -r -c ~/.config/polybar/Multipart/main/config.ini &
+
+if [[ $(xrandr -q | grep "HDMI-A-0 connected") ]]; then
+  sleep 0.1
+  polybar topbar-left -r -c ~/.config/polybar/Multipart/external/config.ini &
+  sleep 0.1
+  polybar topbar-tray -r -c ~/.config/polybar/Multipart/external/config.ini &
+  sleep 0.1
+  polybar topbar-center -r -c ~/.config/polybar/Multipart/external/config.ini &
+  sleep 0.1
+  polybar topbar-beast -r -c ~/.config/polybar/Multipart/external/config.ini &
+  sleep 0.1
+  polybar topbar-right -r -c ~/.config/polybar/Multipart/external/config.ini &
+fi
--- Single/launch.sh
+++ Single/launch.sh
@@ -1,2 +1,6 @@
 sleep 0.1
 polybar single -r -c ~/.config/polybar/Single/config.ini &
+if [[ $(xrandr -q | grep "HDMI-A-0 connected") ]]; then
+  sleep 0.1
+  polybar single -r -c ~/.config/polybar/Single/config-external.ini &
+fi
